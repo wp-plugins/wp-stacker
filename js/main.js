648c5fb0877ceb735db5a04ad829f2f313ea742c
jankarres <jankarres@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-08 14:14:51 +0000

    Upload
    git-svn-id: https://plugins.svn.wordpress.org/wp-stacker/tags/1.3@665226 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/main.js
@@ -0,0 +1,45 @@
+jQuery(document).ready(function () {
+	// If exist, decativate loader and show linklist
+	if (jQuery('.wpstacker .wpstacker_loader').length != 0 && jQuery('.wpstacker #update-nav-menu').length != 0) {
+		jQuery('.wpstacker .wpstacker_loader').fadeOut(250);
+		
+		setTimeout(function() {
+			jQuery('.wpstacker #update-nav-menu').fadeIn(250);
+			jQuery('.wpstacker .support').fadeIn(250);
+		}, 250);
+	}
+	
+	// At submit the form, set the associate in inputs
+	jQuery('#update-nav-menu').submit(function() {
+		// Unbind question
+		window.onbeforeunload = null;
+		
+		jQuery('#update-nav-menu ul li').each(function(key, element) { 
+			var regex = /(.*)menu-item-depth-(.*)/g;
+			var regex2 = /(.*)menu-item-depth-0(.*)/g;
+			
+			if (regex.test(jQuery(element).attr('class')) && !regex2.test(jQuery(element).attr('class'))) {
+				jQuery(this).find('.associate').val('1');
+			}
+		});
+	});
+	
+	// Edit title of a link HTML update
+	jQuery('.menu-item-settings .input-title').keyup(function() {
+		jQuery(this).parents('li').find('.menu-item-handle .left').html(jQuery(this).val());
+	});
+	
+	// Remove a link
+	jQuery('.menu-item-handle .item-remove').click(function() {
+		var answer = confirm('Do you really want to delete this link?');
+		
+		if (answer) {
+			var $this = jQuery(this);
+			$this.parents('li').slideUp(300);
+			
+			setTimeout(function() {
+				$this.parents('li').remove();
+			}, 300);
+		}
+	});
+});
